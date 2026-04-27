764d94fba1f946f2f7e48d7d821a267108d99c41
Lucas Rabelo <lucasrabelodesouza8@gmail.com>
2026-04-27 10:45:41 -0300

    refactor(infra): create InfraModule and put PrismaModule in there to exports

---
+++ src/infra/infra.module.ts
@@ -0,0 +1,35 @@
+import { MailerModule } from "@nestjs-modules/mailer";
+import { HandlebarsAdapter } from "@nestjs-modules/mailer/adapters/handlebars.adapter";
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { PrismaModule } from "./prisma/modules/prisma.module";
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    PrismaModule,
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.MAILTRAP_HOST,
+        port: Number(process.env.MAILTRAP_PORT),
+        secure: false,
+        auth: {
+          user: process.env.MAILTRAP_USERNAME,
+          pass: process.env.MAILTRAP_PASSWORD,
+        },
+      },
+      defaults: {
+        from: '"Login Suporte" <login_suporte@email.com>',
+      },
+      template: {
+        dir: __dirname + '/email/templates',
+        adapter: new HandlebarsAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    })
+  ],
+  exports: [PrismaModule, ConfigModule, MailerModule],
+})
+export class InfraModule { }
