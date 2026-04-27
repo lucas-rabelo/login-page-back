e36ecc72e2761054b2fdf4a1ac81d74276e18cce
Lucas Rabelo <lucasrabelodesouza8@gmail.com>
2026-04-27 10:44:42 -0300

    refactor(hash): created HashModule and import this module in UserModule and AuthModule

---
+++ src/modules/auth/modules/auth.module.ts
@@ -0,0 +1,38 @@
+import { forwardRef, Module } from "@nestjs/common";
+import { JwtModule } from "@nestjs/jwt";
+import { PassportModule } from "@nestjs/passport";
+
+import { AuthController } from "../controllers/auth.controller";
+
+import { CqrsModule } from "@nestjs/cqrs";
+import { HashService } from "../../../shared/hash/services/hash.service";
+import { AuthService } from "../services/auth.service";
+import { GoogleStrategy } from "../strategies/google.strategy";
+
+import { EmailModule } from "../../../shared/email/modules/email.module";
+import { HashModule } from "../../../shared/hash/modules/hash.module";
+import { PrismaModule } from "../../../infra/prisma/modules/prisma.module";
+import { StorageModule } from "../../../shared/storage/modules/storage.module";
+import { TokenModule } from "../../../shared/token/modules/token.module";
+import { UserModule } from "../../user/modules/user.module";
+import { VerifyUserGoogleHandler } from "../domain/command/verify-user-google.handle";
+
+export const CommandHandlers = [VerifyUserGoogleHandler];
+@Module({
+    imports: [
+        CqrsModule,
+        PassportModule.register({ defaultStrategy: 'google' }),
+        PrismaModule,
+        StorageModule,
+        TokenModule,
+        EmailModule,
+        HashModule,
+        UserModule,
+    ],
+    controllers: [AuthController],
+    providers: [AuthService, HashService, GoogleStrategy, ...CommandHandlers],
+    exports: [AuthService, HashService]
+})
+export class AuthModule {
+
+}
---
+++ src/modules/user/modules/user.module.ts
@@ -0,0 +1,40 @@
+import { Module } from "@nestjs/common";
+import { CqrsModule } from "@nestjs/cqrs";
+
+import { PrismaModule } from "../../../infra/prisma/modules/prisma.module";
+import { HashModule } from "../../../shared/hash/modules/hash.module";
+
+import { UserController } from "../controllers/user.controller";
+import { UserService } from "../services/user.service";
+
+import { CreateUserHandler } from "../domain/commands/create-user.handle";
+import { DeleteUserHandler } from "../domain/commands/delete-user.handle";
+import { UpdatePatchUserHandler } from "../domain/commands/update-patch-user.handle";
+import { UpdatePutUserHandler } from "../domain/commands/update-put-user.handle";
+
+import { FindByUuidUserHandler } from "../domain/queries/findByUuid-user.handle";
+import { ListUserHandler } from "../domain/queries/list-user.handle";
+
+export const CommandHandlers = [
+    CreateUserHandler,
+    UpdatePutUserHandler,
+    UpdatePatchUserHandler,
+    DeleteUserHandler
+];
+
+export const QueryHandlers = [
+    ListUserHandler,
+    FindByUuidUserHandler
+];
+
+@Module({
+    imports: [
+        PrismaModule,
+        CqrsModule,
+        HashModule,
+    ],
+    controllers: [UserController],
+    providers: [UserService, ...CommandHandlers, ...QueryHandlers],
+    exports: [UserService]
+})
+export class UserModule { }
