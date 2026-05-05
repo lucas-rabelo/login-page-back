bcdf123ac154c58db9820beecc3cf9a373738dce
Lucas Rabelo <lucasrabelodesouza8@gmail.com>
2026-05-05 16:29:32 -0300

    fix: remove import not used

--- src/auth/modules/auth.module.ts
+++ src/auth/modules/auth.module.ts
@@ -1,5 +1,4 @@
 import { Module } from "@nestjs/common";
-import { JwtModule } from "@nestjs/jwt";
 import { PassportModule } from "@nestjs/passport";
 
 import { AuthController } from "../controllers/auth.controller";
